cdb623452c5f2fbd3a0aae85cf88f414fe457497
NangialaiStoman <nstoman7@gmail.com>
2021-11-24 21:25:19 +0430

    Add projects grid

---
+++ src/components/ProjectsGrid.js
@@ -0,0 +1,100 @@
+const ProjectsGrid = () => {
+	return (
+		<section className="pt-20 sm:pt-28">
+			{/* Projects grid title start */}
+			<div className="text-center">
+				<p className="text-2xl sm:text-5xl font-semibold mb-3 text-ternary-dark dark:text-ternary-light">
+					Projects portfolio
+				</p>
+				<p className="text-md sm:text-xl text-gray-500 dark:text-ternary-light">
+					Some of my projects are listed below
+				</p>
+			</div>
+			{/* Projects grid title end */}
+
+			{/* Filter and search projects start */}
+			<div className="mt-10 sm:mt-16">
+				<h3
+					className="
+          text-center text-secondary-dark
+          dark:text-ternary-light
+          text-md
+          sm:text-xl
+          font-normal
+          mb-3
+        "
+				>
+					Search projects by title or filter by category
+				</h3>
+				<div
+					className="
+          flex
+          justify-between
+          border-b border-primary-light
+          dark:border-secondary-dark
+          pb-3
+          gap-3
+        "
+				>
+					<div className="flex justify-between gap-2">
+						<span
+							className="
+              hidden
+              sm:block
+              bg-primary-light
+              dark:bg-ternary-dark
+              p-2.5
+              shadow-sm
+              rounded-xl
+              cursor-pointer
+            "
+						>
+							<i
+								data-feather="search"
+								className="text-ternary-dark dark:text-ternary-light"
+							></i>
+						</span>
+						<input
+							className="
+                                pl-3
+                                pr-1
+                                sm:px-4
+                                py-2
+                                border-1 border-gray-200
+                                dark:border-secondary-dark
+                                rounded-lg
+                                text-sm
+                                sm:text-md
+                                bg-secondary-light
+                                dark:bg-ternary-dark
+                                text-primary-dark
+                                dark:text-ternary-light
+                                "
+							id="name"
+							name="name"
+							type="search"
+							required=""
+							placeholder="Search Projects"
+							aria-label="Name"
+						/>
+					</div>
+					{/* <ProjectsFilter @change="selectedProject = $event" /> */}
+				</div>
+			</div>
+			{/* Filter and search projects end */}
+
+			{/* Projects grid start */}
+			<div className="grid grid-cols-1 sm:grid-cols-3 mt-6 sm:gap-10">
+				{/* <ProjectSingle
+				v-for="project in filteredProjects"
+				:key="project.id"
+				:project="project"
+			/> */}
+				A single project
+			</div>
+			{/* Projects grid end */}
+		</section>
+	);
+};
+
+export default ProjectsGrid;
