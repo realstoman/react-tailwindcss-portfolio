a3a82ccb622d78c1a8e4fa78f4dcc3ad014c712b
NangialaiStoman <nstoman7@gmail.com>
2021-11-24 21:06:05 +0430

    Add footer layout and ui

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import './css/App.css';
 import AppHeader from './components/AppHeader';
+import AppFooter from './components/AppFooter';
 
 function App() {
 	return (
 		<div className="App">
 			<AppHeader></AppHeader>
+
+			<AppFooter></AppFooter>
 		</div>
 	);
 }
