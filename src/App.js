72b39debecefe7c2a8931252f3fdd42196bbf1a8
NangialaiStoman <nstoman7@gmail.com>
2021-11-24 21:21:24 +0430

    App home to app js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import './css/App.css';
 import AppHeader from './components/AppHeader';
 import AppFooter from './components/AppFooter';
+import Home from './pages';
 
 function App() {
 	return (
 		<div className="App">
 			<AppHeader></AppHeader>
-
+			<Home />
 			<AppFooter></AppFooter>
 		</div>
 	);
