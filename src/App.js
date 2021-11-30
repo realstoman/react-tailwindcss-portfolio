a6723c18bc650d50793eb2625715011160d5d07b
NangialaiStoman <nstoman7@gmail.com>
2021-11-30 10:46:42 +0430

    React routes and about page

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './css/App.css';
 import AppHeader from './components/AppHeader';
 import AppFooter from './components/AppFooter';
-import Home from './pages';
+import Home from './pages/Home';
+import About from './pages/about';
 
 function App() {
 	return (
 		<div className="App">
 			<Router>
 				<AppHeader></AppHeader>
-				<Home />
+				<Routes>
+					<Route path="/" element={<Home />} />
+					<Route path="about" element={<About />} />
+				</Routes>
 				<AppFooter></AppFooter>
 			</Router>
 		</div>
---
+++ src/pages/About.js
@@ -0,0 +1,9 @@
+const About = () => {
+	return (
+		<div>
+			<h1>This is the About me page</h1>
+		</div>
+	);
+};
+
+export default About;
