9b3787f980848406e1482fd752597e8929bcfefa
NangialaiStoman <nstoman7@gmail.com>
2021-11-30 10:47:35 +0430

    Create pages

---
+++ src/pages/Contact.js
@@ -0,0 +1,9 @@
+const Contact = () => {
+	return (
+		<div>
+			<h1>This is the contact page</h1>
+		</div>
+	);
+};
+
+export default Contact;
---
+++ src/pages/Projects.js
@@ -0,0 +1,9 @@
+const Projects = () => {
+	return (
+		<div>
+			<h1>This is the projects page</h1>
+		</div>
+	);
+};
+
+export default Projects;
