e8940edcc8f790d07567c743ae20eae03ce416be
NangialaiStoman <nstoman7@gmail.com>
2021-11-30 11:13:48 +0430

    Add contact form

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,6 +1,10 @@
+import ContactForm from '../components/ContactForm';
+
 const Contact = () => {
 	return (
-		<div className="container mx-auto sm:flex py-5 sm:py-10 mt-10 sm:mt-20"></div>
+		<div className="container mx-auto sm:flex py-5 sm:py-10 mt-10 sm:mt-20">
+			<ContactForm />
+		</div>
 	);
 };
 
