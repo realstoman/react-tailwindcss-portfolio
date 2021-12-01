e81b7fceee6d046115921d9cc854e4ee8c8df038
NangialaiStoman <nstoman7@gmail.com>
2021-12-01 16:43:03 +0430

    Small screen toggle menu

--- src/components/AppHeader.js
+++ src/components/AppHeader.js
@@ -4,9 +4,13 @@ import useThemeSwitcher from '../hooks/useThemeSwitcher';
 
 import logoLight from '../images/logo-light.svg';
 import logoDark from '../images/logo-dark.svg';
+import { useState } from 'react';
 
 const AppHeader = () => {
+	const [showMenu, setShowMenu] = useState(false);
 	const [activeTheme, setTheme] = useThemeSwitcher();
+
+	const toggleMenu = showMenu === false ? true : false;
 
 	return (
 		<div>
@@ -15,6 +19,7 @@ const AppHeader = () => {
 				<div className="z-10 max-w-screen-lg xl:max-w-screen-xl block sm:flex sm:justify-between sm:items-center py-6">
 					{/* Header menu links and small screen hamburger menu start */}
 					<div className="flex justify-between items-center px-4 sm:px-0">
+						{/* Logo start */}
 						<div>
 							<Link to="/">
 								{activeTheme === 'dark' ? (
@@ -32,6 +37,7 @@ const AppHeader = () => {
 								)}
 							</Link>
 						</div>
+						{/* Logo end */}
 
 						{/* Theme switcher small screen start */}
 						<div
@@ -50,6 +56,7 @@ const AppHeader = () => {
 						{/* Small screen hamburger menu start */}
 						<div className="sm:hidden">
 							<button
+								onClick={() => setShowMenu(toggleMenu)}
 								type="button"
 								className="focus:outline-none"
 								aria-label="Hamburger Menu"
@@ -60,13 +67,11 @@ const AppHeader = () => {
 									className="h-7 w-7 fill-current text-secondary-dark dark:text-ternary-light"
 								>
 									<path
-										v-if="isOpen"
 										fill-rule="evenodd"
 										d="M18.278 16.864a1 1 0 0 1-1.414 1.414l-4.829-4.828-4.828 4.828a1 1 0 0 1-1.414-1.414l4.828-4.829-4.828-4.828a1 1 0 0 1 1.414-1.414l4.829 4.828 4.828-4.828a1 1 0 1 1 1.414 1.414l-4.828 4.829 4.828 4.828z"
 										clip-rule="evenodd"
 									></path>
 									<path
-										v-if="!isOpen"
 										fill-rule="evenodd"
 										d="M4 5h16a1 1 0 0 1 0 2H4a1 1 0 1 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2z"
 									></path>
