0024c5f3e2478f291d26b778faf971b8ba218767
realstoman <realstoman@gmail.com>
2022-01-19 16:26:29 +0430

    Project grid fonts

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -12,10 +12,10 @@ const Home = () => {
 				<ProjectsGrid></ProjectsGrid>
 			</ProjectsProvider>
 
-			<div className="mt-10 sm:mt-15 flex justify-center">
+			<div className="mt-8 sm:mt-10 flex justify-center">
 				<Link
 					to="/projects"
-					className="flex items-center px-6 py-3 rounded-lg shadow-lg hover:shadow-xl bg-indigo-500 hover:bg-indigo-600 focus:ring-1 focus:ring-indigo-900 text-white text-lg sm:text-xl font-medium"
+					className="font-general-medium flex items-center px-6 py-3 rounded-lg shadow-lg hover:shadow-xl bg-indigo-500 hover:bg-indigo-600 focus:ring-1 focus:ring-indigo-900 text-white text-lg sm:text-xl"
 					aria-label="More Projects"
 				>
 					More Projects
