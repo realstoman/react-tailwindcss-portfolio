4355c8b0f5feb89c1b04c72aea46cc31e05a675c
realstoman <realstoman@gmail.com>
2022-01-19 16:33:24 +0430

    About fonts and heading updates

--- src/data/clientsData.js
+++ src/data/clientsData.js
@@ -8,7 +8,7 @@ import SamsungImage from '../images/brands/samsung_gray.png';
 import CanonImage from '../images/brands/canon_gray.png';
 import PumaImage from '../images/brands/puma_gray.png';
 
-export const clientsHeading = 'Some of the brands I worked with';
+export const clientsHeading = 'Some of the brands that trust me';
 
 export const clientsData = [
 	{
