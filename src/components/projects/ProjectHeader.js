d42a9e4a079d5c70045aad27c860985c2ea99fe7
realstoman <realstoman@gmail.com>
2022-01-19 16:53:00 +0430

    Single project fonts

--- src/components/projects/ProjectHeader.js
+++ src/components/projects/ProjectHeader.js
@@ -7,19 +7,19 @@ const ProjectSingleHeader = () => {
 
 	return (
 		<div>
-			<p className="text-left text-3xl sm:text-4xl font-bold text-primary-dark dark:text-primary-light mt-7 sm:mt-20 mb-7">
+			<p className="font-general-medium text-left text-3xl sm:text-4xl font-bold text-primary-dark dark:text-primary-light mt-14 sm:mt-20 mb-7">
 				{singleProjectData.ProjectHeader.title}
 			</p>
 			<div className="flex">
 				<div className="flex items-center mr-10">
-					<FiClock className="text-xl text-ternary-dark dark:text-ternary-light" />
-					<span className="ml-2 leading-none text-primary-dark dark:text-primary-light">
+					<FiClock className="text-lg text-ternary-dark dark:text-ternary-light" />
+					<span className="font-general-regular ml-2 leading-none text-primary-dark dark:text-primary-light">
 						{singleProjectData.ProjectHeader.publishDate}
 					</span>
 				</div>
 				<div className="flex items-center">
-					<FiTag className="w-4 h-4 text-ternary-dark dark:text-ternary-light" />
-					<span className="ml-2 leading-none text-primary-dark dark:text-primary-light">
+					<FiTag className="text-lg text-ternary-dark dark:text-ternary-light" />
+					<span className="font-general-regular ml-2 leading-none text-primary-dark dark:text-primary-light">
 						{singleProjectData.ProjectHeader.tags}
 					</span>
 				</div>
