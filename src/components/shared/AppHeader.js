24093ab44169ed510414a88744354d5c9e17d0b5
realstoman <realstoman@gmail.com>
2022-01-19 16:21:48 +0430

    Header navigation and banner fonts

--- src/components/shared/AppHeader.js
+++ src/components/shared/AppHeader.js
@@ -145,8 +145,8 @@ const AppHeader = () => {
 				</div>
 				{/* Header links small screen end */}
 
-				{/* Header links small screen start*/}
-				<div className="hidden m-0 sm:ml-4 mt-5 sm:mt-3 sm:flex p-5 sm:p-0 justify-center items-center shadow-lg sm:shadow-none">
+				{/* Header links large screen start*/}
+				<div className="font-general-medium hidden m-0 sm:ml-4 mt-5 sm:mt-3 sm:flex p-5 sm:p-0 justify-center items-center shadow-lg sm:shadow-none">
 					<Link
 						to="/projects"
 						className="block text-left text-lg font-medium text-primary-dark dark:text-ternary-light hover:text-secondary-dark dark:hover:text-secondary-light  sm:mx-4 mb-2 sm:py-2"
