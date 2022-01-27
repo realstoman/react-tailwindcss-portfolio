0eb094979b4a19ac1d58410c3ab99013636001a2
realstoman <realstoman@gmail.com>
2022-01-27 17:23:00 +0430

    Contact details and contact form refactoring

--- src/components/contact/ContactDetails.js
+++ src/components/contact/ContactDetails.js
@@ -20,15 +20,15 @@ const contacts = [
 
 const ContactDetails = () => {
 	return (
-		<div className="font-general-regular w-full lg:w-1/2">
+		<div className="w-full lg:w-1/2">
 			<div className="text-left max-w-xl px-6">
-				<h2 className="text-2xl text-primary-dark dark:text-primary-light font-semibold mt-12 mb-8">
+				<h2 className="font-general-medium text-2xl text-primary-dark dark:text-primary-light mt-12 mb-8">
 					Contact details
 				</h2>
-				<ul>
+				<ul className="font-general-regular">
 					{contacts.map((contact) => (
 						<li className="flex " key={contact.id}>
-							<i className="text-2xl text-gray-500 dark:text-gray-400 mr-4 mt-1">
+							<i className="text-2xl text-gray-500 dark:text-gray-400 mr-4">
 								{contact.icon}
 							</i>
 							<span className="text-lg mb-4 text-ternary-dark dark:text-ternary-light">
