d007dd723a1ed2671db98f864b37051be30168f6
realstoman <realstoman@gmail.com>
2022-04-03 22:17:03 +0430

    Add new react 18 createRoot hook

--- src/index.js
+++ src/index.js
@@ -1,15 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+// import React from 'react';
+// import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-	<React.StrictMode>
-		<App />
-	</React.StrictMode>,
-	document.getElementById('root')
-);
+import { createRoot } from 'react-dom/client';
+const container = document.getElementById('root');
+const root = createRoot(container);
+root.render(<App />);
+
+// ReactDOM.render(
+// 	<React.StrictMode>
+// 		<App />
+// 	</React.StrictMode>,
+// 	document.getElementById('root')
+// );
+
+// root.render(
+// 	<React.StrictMode>
+// 		<App />
+// 	</React.StrictMode>
+// );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
