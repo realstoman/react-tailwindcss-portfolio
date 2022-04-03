a938da0c3d846c7e93d4ab8f64b5e395b3760dbf
realstoman <realstoman@gmail.com>
2022-04-03 22:17:50 +0430

    Code cleanup

--- src/index.js
+++ src/index.js
@@ -1,5 +1,3 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
@@ -9,20 +7,4 @@ const container = document.getElementById('root');
 const root = createRoot(container);
 root.render(<App />);
 
-// ReactDOM.render(
-// 	<React.StrictMode>
-// 		<App />
-// 	</React.StrictMode>,
-// 	document.getElementById('root')
-// );
-
-// root.render(
-// 	<React.StrictMode>
-// 		<App />
-// 	</React.StrictMode>
-// );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
