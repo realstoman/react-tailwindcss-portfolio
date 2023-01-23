d79d80ae23aa3e26f80c4111162a2f5dfefc8787
Subham Bhattacharjee <94003231+SubhamB2003@users.noreply.github.com>
2023-01-23 16:10:56 +0530

    Bug solved

--- src/App.js
+++ src/App.js
@@ -1,15 +1,30 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { AnimatePresence } from 'framer-motion';
+import { lazy, Suspense } from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import ScrollToTop from './components/ScrollToTop';
 import AppFooter from './components/shared/AppFooter';
 import AppHeader from './components/shared/AppHeader';
 import './css/App.css';
-import About from './pages/AboutMe';
-import Contact from './pages/Contact.jsx';
-import Home from './pages/Home';
-import Projects from './pages/Projects';
-import ProjectSingle from './pages/ProjectSingle.jsx';
-import { AnimatePresence } from 'framer-motion';
 import UseScrollToTop from './hooks/useScrollToTop';
+
+const About = lazy(() => import('./pages/AboutMe'));
+const Contact = lazy(() => import('./pages/Contact.jsx'));
+const Home = lazy(() => import('./pages/Home'));
+const Projects = lazy(() => import('./pages/Projects'));
+const ProjectSingle = lazy(() => import('./pages/ProjectSingle.jsx'));
+
+
+const Loading = () => {
+	return (
+		<div className='flex justify-center pt-10' role="status">
+			<svg aria-hidden="true" class="w-12 h-12 mr-2 text-gray-200 animate-spin fill-blue-600" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+				<path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor" />
+				<path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill" />
+			</svg>
+		</div>
+
+	)
+}
 
 function App() {
 	return (
@@ -18,17 +33,19 @@ function App() {
 				<Router>
 					<ScrollToTop />
 					<AppHeader />
-					<Routes>
-						<Route path="/" element={<Home />} />
-						<Route path="projects" element={<Projects />} />
-						<Route
-							path="projects/single-project"
-							element={<ProjectSingle />}
-						/>
+					<Suspense fallback={<Loading />}>
+						<Routes>
+							<Route path="/" element={<Home />} />
+							<Route path="projects" element={<Projects />} />
+							<Route
+								path="projects/single-project"
+								element={<ProjectSingle />}
+							/>
 
-						<Route path="about" element={<About />} />
-						<Route path="contact" element={<Contact />} />
-					</Routes>
+							<Route path="about" element={<About />} />
+							<Route path="contact" element={<Contact />} />
+						</Routes>
+					</Suspense>
 					<AppFooter />
 				</Router>
 				<UseScrollToTop />
