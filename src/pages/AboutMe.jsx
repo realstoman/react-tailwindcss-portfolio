0ae58cd054b1bbb8917fb714b53619fc053a805e
Jonas Rosengren <rosengrenjonas98@gmail.com>
2023-11-13 22:37:29 +0100

    rm countUp

--- src/pages/AboutMe.jsx
+++ src/pages/AboutMe.jsx
@@ -1,5 +1,4 @@
 import AboutMeBio from '../components/about/AboutMeBio';
-import AboutCounter from '../components/about/AboutCounter';
 import AboutClients from '../components/about/AboutClients';
 import { AboutMeProvider } from '../context/AboutMeContext';
 import { motion } from 'framer-motion';
